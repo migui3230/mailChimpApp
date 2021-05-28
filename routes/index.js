6f61d2c338331770511c906709e132c131e240d5
migui3230 <migui0323@gmail.com>
2021-05-28 17:03:09 -0400

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+var https = require("https")
+var bodyParser = require("body-parser")
+var mailchimp = require("@mailchimp/mailchimp_marketing")
+
+mailchimp.setConfig({
+  apiKey: "448117d29eaf23eba8cc0ed7b8edf3a6-us6",
+  server: "us6",
+});
+
+router.use(bodyParser.urlencoded({extended: true}))
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(__dirname + "/signup.html")
+});
+
+router.post("/", function (req, res) {
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName
+  const email = req.body.email
+
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+  }
+
+  const run = async () => {
+    try {
+      const response = await mailchimp.lists.addListMember("484c96ad6e", {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName
+        }
+      });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html")
+    } catch (err) {
+      console.log(err.status);
+      // console.log("====== ERROR ======");
+      // console.log(JSON.parse(err.response.error.text).detail)
+      res.sendFile(__dirname + "/failure.html")
+    }
+  };
+
+  run()
+})
+
+router.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+
+module.exports = router;
