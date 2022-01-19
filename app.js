d2341d65454e14d019dc9df31a798be22c491e9b
migui3230 <74937076+migui3230@users.noreply.github.com>
2022-01-18 21:23:36 -0500

    added documentation

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// middleware
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,12 +16,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// configs
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes to the signup page
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -40,6 +44,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// uses the public static file within the file structure
 app.use(express.static("public"))
 
 // process.env.PORT allows heroku to set the port of the app to their server
